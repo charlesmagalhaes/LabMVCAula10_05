fed63fba0c93b07e0b97055e0c21190a925f25cb
Charles Magalhaes <charlesrm29@gmail.com>
2023-05-25 15:41:21 -0300

    Versao 1 projeto

---
+++ LabMVCAula10_05/Controllers/ApiService.cs
@@ -0,0 +1,49 @@
+﻿using LabMVCAula10_05.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LabMVCAula10_05.Controllers
+{
+
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<ProdutoModel>> Get(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                List<ProdutoModel> listaResponse = JsonConvert.DeserializeObject<List<ProdutoModel>>(jsonResponse);
+                return listaResponse;
+            }
+            else
+            {
+                // Lidar com o erro de acordo com a necessidade do seu aplicativo
+                throw new Exception($"Erro ao obter os produtos. StatusCode: {response.StatusCode}");
+            }
+        }
+
+
+        public async Task<HttpResponseMessage> Salvar(string url, ProdutoModel produto)
+        {
+            var jsonContent = new StringContent(JsonConvert.SerializeObject(produto), Encoding.UTF8, "application/json");
+            return await _httpClient.PostAsync(url, jsonContent);
+        }
+
+    }
+
+}
---
+++ LabMVCAula10_05/Controllers/ProdutoController.cs
@@ -0,0 +1,43 @@
+﻿using LabMVCAula10_05.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabMVCAula10_05.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ApiService _apiService = new ApiService();
+
+        public ProdutoController()
+        {
+            
+        }
+
+        public async Task<ActionResult> Produto()
+        {
+            ViewBag.ListaProdutos = await _apiService.Get("https://localhost:44395/api/values");
+
+            return View(new ProdutoModel());
+        }
+
+
+        public async Task<ActionResult> Salvar(ProdutoModel produto)
+        {
+
+            var response = await _apiService.Salvar("https://localhost:44395/api/values", produto);
+            var responseData = await response.Content.ReadAsStringAsync();
+            
+
+            // Trate a resposta recebida da API e retorne para a exibição
+            return RedirectToAction("Produto");
+        }
+
+    }
+}
